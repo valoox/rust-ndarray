cb90f2a2747b605aefe6dfb7102c499692542267
bluss <bluss@users.noreply.github.com>
2015-12-14 11:13:36 +0100

    Update docs

---
+++ doc/implementors/core/ops/trait.AddAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ndarray'] = ["impl&lt;'a, A, D, E&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html' title='core::ops::AddAssign'>AddAssign</a>&lt;&amp;'a <a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, E&gt;&gt; for <a class='struct' href='ndarray/struct.Array.html' title='ndarray::Array'>Array</a>&lt;A, D&gt; <span class='where'>where A: <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html' title='core::ops::AddAssign'>AddAssign</a>&lt;A&gt;, D: <a class='trait' href='ndarray/trait.Dimension.html' title='ndarray::Dimension'>Dimension</a>, E: <a class='trait' href='ndarray/trait.Dimension.html' title='ndarray::Dimension'>Dimension</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
