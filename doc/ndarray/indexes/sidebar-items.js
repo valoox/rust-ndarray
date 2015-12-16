64f6938d69a9b5be11f5a13cee43a71e15bb6679
bluss <bluss@users.noreply.github.com>
2015-12-16 22:02:26 +0100

    Update docs

--- doc/ndarray/indexes/sidebar-items.js
+++ doc/ndarray/indexes/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Indexes","An iterator of the indexes of an array shape."]]});
+initSidebarItems({"struct":[["Indexes","An iterator over the indexes of an array shape."]]});
