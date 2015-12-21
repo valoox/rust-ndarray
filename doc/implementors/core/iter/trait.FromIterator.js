8b2b52e613909da3c70bfe995ea021cff0b75f7f
bluss <bluss@users.noreply.github.com>
2015-12-21 11:01:53 +0100

    Update docs

--- doc/implementors/core/iter/trait.FromIterator.js
+++ doc/implementors/core/iter/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['ndarray'] = ["impl&lt;A&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;A&gt; for <a class='type' href='ndarray/type.Array.html' title='ndarray::Array'>Array</a>&lt;A, <a class='type' href='ndarray/type.Ix.html' title='ndarray::Ix'>Ix</a>&gt;",];
+implementors['ndarray'] = ["impl&lt;A, S&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;A&gt; for <a class='struct' href='ndarray/struct.ArrayBase.html' title='ndarray::ArrayBase'>ArrayBase</a>&lt;S, <a class='type' href='ndarray/type.Ix.html' title='ndarray::Ix'>Ix</a>&gt; <span class='where'>where S: <a class='trait' href='ndarray/trait.DataOwned.html' title='ndarray::DataOwned'>DataOwned</a>&lt;Elem=A&gt;</span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
