3a707969ab7d0d22225114a5f22953d52c2ff78e
bluss <bluss@users.noreply.github.com>
2016-01-29 00:01:36 +0100

    Update docs

--- master/implementors/std/error/trait.Error.js
+++ master/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['ndarray'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='ndarray/enum.ShapeError.html' title='ndarray::ShapeError'>ShapeError</a>",];
+implementors['ndarray'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='ndarray/enum.ShapeError.html' title='ndarray::ShapeError'>ShapeError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='ndarray/enum.StrideError.html' title='ndarray::StrideError'>StrideError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
